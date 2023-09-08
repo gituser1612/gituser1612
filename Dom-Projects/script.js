43eb5bf8e0b033b30c246250b27e44246db89386
Amit Kumar Pandey <138184479+gituser1612@users.noreply.github.com>
2023-09-08 23:18:53 +0530

    Add files via upload
    hey, this was an awesome project from pure html css and js. It is basically a custom cursor effect.

---
+++ Dom-Projects/script.js
@@ -0,0 +1,11 @@
+
+
+// Project-3 Custom Cursor 
+
+var main = document.querySelector("#main")
+var crsr =  document.querySelector(".cursor")
+
+ main.addEventListener("mousemove", function(dets){
+    crsr.style.left =  dets.x+"px"
+    crsr.style.top =  dets.y+"px"
+ })
